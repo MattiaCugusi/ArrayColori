c403b20e40c5e9c2c82cc6e218911567b4fb99d4
Mattia Cugusi <m.cugusi287gmail.com>
2024-10-05 19:31:10 +0200

    esercizio completato

--- ArrayColori.php
+++ ArrayColori.php
@@ -6,6 +6,30 @@
     <title>Array Colori</title>
 </head>
 <body>
+    <?php
+      $num = rand(10,20);
+
+      $lista = [];
+
+      for ($i = 0; $i <= $num; $i++){
+        $lista[$i] = $i;
+      }
+
+      $colori = ["red", "black", "green", "purple", "blue"];
+
+      $cas = rand (0, 4);
+
+      echo "<ol>";
+
+      for ($i = 0; $i < $num ; $i++){
+        echo "<li style = 'color: " . $colori[$cas] . "'>" . $lista[$i] . "</li>";
+      }
+
+      echo "</ol>";
+
+      
+    ?>
+
     
 </body>
 </html>
